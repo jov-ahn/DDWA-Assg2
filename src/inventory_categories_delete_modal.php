a5cd382c8961924b062a36c3c2b51ce55af36646
Misty <jov4hn@gmail.com>
2021-02-07 00:27:50 +0800

    1.0.0
    - Added CRUD for inventory and inventory categories page
    - Made changes to sidebar
    - Refactored all db codes

---
+++ src/inventory_categories_delete_modal.php
@@ -0,0 +1,23 @@
+<div class="modal fade" id="deleteModal<?= $row['category_id'] ?>" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteModalLabel">Delete Inventory Category</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="inventory_categories_db.php" method="POST" class="needs-validation" novalidate>
+                <div class="modal-body">
+                    <input type="hidden" name="category-id" value="<?= $row['category_id'] ?>">
+                    <h4>Delete "<?= $row['category_name'] ?>"?</h4>
+                    <h6>This action cannot be undone.</h6>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" name="delete-inventory-category" class="btn btn-primary">Confirm</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ src/inventory_categories_edit_modal.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="editModal<?= $row['category_id'] ?>" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editModalLabel">Edit Inventory Category</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="inventory_categories_db.php" method="POST" class="needs-validation" novalidate>
+                <div class="modal-body">
+                    <input type="hidden" name="category-id" value="<?= $row['category_id'] ?>">
+                    <div class="form-group">
+                        <label>Category Name</label>
+                        <input type="text" name="category-name" value="<?= $row['category_name'] ?>" class="form-control" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" name="edit-inventory-category" class="btn btn-primary">Save Changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
